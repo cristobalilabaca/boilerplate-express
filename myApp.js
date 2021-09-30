e9fd05cac06b589f3a4fcce783b281def442ada1
Cristobal ilabaca <cilabaca1@uc.cl>
2021-09-30 16:24:23 -0300

    first json response

--- myApp.js
+++ myApp.js
@@ -7,6 +7,10 @@ app.get('/', (_, res) => {
   res.sendFile(__dirname + '/views/index.html');
 })
 
+app.get("/json", (_, res) => {
+  res.json({"message": "Hello json"});
+})
+
 console.log("Hello World");
 
 
